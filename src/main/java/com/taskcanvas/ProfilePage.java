5780f6ddb6d8fb647b39fb2fb56d1874e92ea4e4
kamegoro <tentyo0620@icloud.com>
2024-10-31 21:34:24 +0900

    update page

--- src/main/java/com/taskcanvas/ProfilePage.java
+++ src/main/java/com/taskcanvas/ProfilePage.java
@@ -6,6 +6,7 @@ import org.apache.wicket.markup.html.basic.Label;
 public class ProfilePage extends  WebPage {
     public ProfilePage() {
         add(new Label("name", "山田太郎"));
+        add(new Label("mailAddress", "test@test.com"));
         add(new Label("age", "25"));
         add(new Label("gender", "男"));
         add(new Label("address", "東京都港区"));
