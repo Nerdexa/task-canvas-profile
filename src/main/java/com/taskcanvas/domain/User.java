a04de2510d7a04dada16fd47ee629f759a3bf107
kamegoro <tentyo0620@icloud.com>
2024-11-03 04:50:35 +0900

    update: domain

--- src/main/java/com/taskcanvas/domain/User.java
+++ src/main/java/com/taskcanvas/domain/User.java
@@ -1,12 +1,15 @@
 package com.taskcanvas.domain;
 
+import lombok.Data;
+
 import java.util.UUID;
 
+@Data
 public class User {
     private final UUID id;
     private final String mailAddress;
 
-    public User(UUID id,String mailAddress) {
+    public User(UUID id, String mailAddress) {
         this.id = id;
         this.mailAddress = mailAddress;
     }
@@ -16,6 +19,10 @@ public class User {
 
         public Id(UUID value) {
             this.value = value;
+        }
+
+        public UUID getValue() {
+            return value;
         }
     }
 }
